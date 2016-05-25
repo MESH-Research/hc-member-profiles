b73d6db70769ad2db20c6a1a8df00db10bc6a345
Ryan Williams <ryrowi@gmail.com>
2016-05-25 17:13:12 -0400

    activate bp-block-member in setup script

--- bash/setup.bash
+++ bash/setup.bash
@@ -2,5 +2,6 @@
 set -ex
 
 wp plugin deactivate --network cac-advanced-profiles
-wp plugin activate --network profile
 wp plugin activate --network buddypress-followers
+wp plugin activate --network bp-block-member
+wp plugin activate --network profile
