7c8ca3460ca4837773496e1466830dcfe8d3007c
Ryan Williams <ryrowi@gmail.com>
2016-06-13 09:41:02 -0400

    simplify disable_bp_component

--- src/Profile.php
+++ src/Profile.php
@@ -14,14 +14,14 @@ class Profile extends BP_Component {
 	public $template_files;
 
 	public function __construct() {
-		$this->plugin_dir = plugin_dir_path( __DIR__ . '/../..' );
-		$this->plugin_templates_dir = trailingslashit( $this->plugin_dir . 'templates' );
+		$this->plugin_dir = \plugin_dir_path( __DIR__ . '/../..' );
+		$this->plugin_templates_dir = \trailingslashit( $this->plugin_dir . 'templates' );
 		$this->template_files = new RecursiveIteratorIterator(
 			new RecursiveDirectoryIterator( $this->plugin_templates_dir ),
 			RecursiveIteratorIterator::SELF_FIRST
 		);
 
-		add_action( 'bp_init', [ $this, 'init' ] );
+		\add_action( 'bp_init', [ $this, 'init' ] );
 	}
 
 	public static function get_instance() {
@@ -32,10 +32,10 @@ class Profile extends BP_Component {
 	 * TODO check if required plugins are active & throw warning or bail if not: follow, block
 	 */
 	public function init() {
-		if ( ! bp_is_user_change_avatar() && ( bp_is_user_profile() || bp_is_user_profile_edit() ) ) {
-			add_filter( 'load_template', [ $this, 'filter_load_template' ] );
-			add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
-			add_action( 'xprofile_updated_profile', [ $this, 'save_academic_interests' ] );
+		if ( ! \bp_is_user_change_avatar() && ( \bp_is_user_profile() || \bp_is_user_profile_edit() ) ) {
+			\add_filter( 'load_template', [ $this, 'filter_load_template' ] );
+			\add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+			\add_action( 'xprofile_updated_profile', [ $this, 'save_academic_interests' ] );
 		}
 
 		// disable buddypress friends component in favor of follow/block
@@ -45,19 +45,19 @@ class Profile extends BP_Component {
 	public function disable_bp_component( $component_name ) {
 		$active_components = \bp_get_option( 'bp-active-components' );
 
-		if ( in_array( $component_name, array_keys( $active_components ) ) ) {
+		if ( isset( $active_components[$component_name] ) ) {
 			unset( $active_components[$component_name] );
-			bp_update_option( 'bp-active-components', $active_components );
+			\bp_update_option( 'bp-active-components', $active_components );
 		}
 	}
 
 	public function enqueue_scripts() {
-		wp_enqueue_style( 'mla_commons_profile_main_css', plugins_url() . '/profile/css/main.css' );
-		wp_enqueue_script( 'mla_commons_profile_main_js', plugins_url() . '/profile/js/main.js' );
+		\wp_enqueue_style( 'mla_commons_profile_main_css', \plugins_url() . '/profile/css/main.css' );
+		\wp_enqueue_script( 'mla_commons_profile_main_js', \plugins_url() . '/profile/js/main.js' );
 	}
 
 	public function filter_load_template( $path ) {
-		$their_slug = str_replace( trailingslashit( STYLESHEETPATH ), '', $path );
+		$their_slug = str_replace( \trailingslashit( STYLESHEETPATH ), '', $path );
 
 		foreach( $this->template_files as $name => $object ){
 			$our_slug = str_replace( $this->plugin_templates_dir, '', $name );
@@ -68,6 +68,35 @@ class Profile extends BP_Component {
 		}
 
 		return $path;
+	}
+
+	public function save_academic_interests( $user_id ) {
+		$tax = \get_taxonomy( 'mla_academic_interests' );
+
+		// If array add any new keywords.
+		if ( is_array( $_POST['academic-interests'] ) ) {
+			foreach ( $_POST['academic-interests'] as $term_id ) {
+				$term_key = \term_exists( $term_id, 'mla_academic_interests' );
+				if ( empty( $term_key ) ) {
+					$term_key = \wp_insert_term( \sanitize_text_field( $term_id ), 'mla_academic_interests' );
+				}
+				if ( ! \is_wp_error( $term_key ) ) {
+					$term_ids[] = intval( $term_key['term_id'] );
+				} else {
+					error_log( '*****CAC Academic Interests Error - bad tag*****' . var_export( $term_key, true ) );
+				}
+			}
+		}
+
+		// Set object terms for tags.
+		$term_taxonomy_ids = \wp_set_object_terms( $user_id, $term_ids, 'mla_academic_interests' );
+		\clean_object_term_cache( $user_id, 'mla_academic_interests' );
+
+		// Set user meta for theme query.
+		\delete_user_meta( $user_id, 'academic_interests' );
+		foreach ( $term_taxonomy_ids as $term_taxonomy_id ) {
+			\add_user_meta( $user_id, 'academic_interests', $term_taxonomy_id, $unique = false );
+		}
 	}
 
 }
--- src/Profile/Template.php
+++ src/Profile/Template.php
@@ -13,7 +13,7 @@ class Template {
 		$follow_counts = 0;
 
 		if ( function_exists( 'bp_follow_total_follow_counts' ) ) {
-			$follow_counts = bp_follow_total_follow_counts();
+			$follow_counts = \bp_follow_total_follow_counts();
 		} else {
 			// TODO log error
 		}
@@ -22,12 +22,12 @@ class Template {
 	}
 
 	public function get_academic_interests() {
-		$tax = get_taxonomy( 'mla_academic_interests' );
-		$interests = wp_get_object_terms( bp_displayed_user_id(), 'mla_academic_interests', array( 'fields' => 'names' ) );
+		$tax = \get_taxonomy( 'mla_academic_interests' );
+		$interests = \wp_get_object_terms( \bp_displayed_user_id(), 'mla_academic_interests', array( 'fields' => 'names' ) );
 		$html = '<ul>';
 		foreach ( $interests as $term_name ) {
-			$search_url = add_query_arg( array( 's' => urlencode( $term_name ) ), bp_get_members_directory_permalink() );
-			$html .= '<li><a href="' . esc_url( $search_url ) . '" rel="nofollow">';
+			$search_url = \add_query_arg( array( 's' => urlencode( $term_name ) ), \bp_get_members_directory_permalink() );
+			$html .= '<li><a href="' . \esc_url( $search_url ) . '" rel="nofollow">';
 			$html .=  $term_name;
 			$html .= '</a></li>';
 		}
@@ -38,16 +38,17 @@ class Template {
 	public function get_academic_interests_edit() {
 		global $mla_academic_interests;
 
-		$tax = get_taxonomy( 'mla_academic_interests' );
+		$tax = \get_taxonomy( 'mla_academic_interests' );
+
+		$interest_list = $mla_academic_interests->mla_academic_interests_list();
+		$input_interest_list = \wp_get_object_terms( \bp_displayed_user_id(), 'mla_academic_interests', [ 'fields' => 'names' ] );
 
 		$html = '<p class="description">Enter interests from the existing list, or add new interests if needed.</p>';
 		$html .= '<select name="academic-interests[]" class="js-basic-multiple-tags interests" multiple="multiple" data-placeholder="Enter interests.">';
 
-		$interest_list = $mla_academic_interests->mla_academic_interests_list();
-		$input_interest_list = wp_get_object_terms( bp_displayed_user_id(), 'mla_academic_interests', array( 'fields' => 'names' ) );
-
 		foreach ( $interest_list as $interest_key => $interest_value ) {
-			$html .= sprintf('			<option class="level-1" %1$s value="%2$s">%3$s</option>' . "\n",
+			$html .= sprintf('
+				<option class="level-1" %1$s value="%2$s">%3$s</option>' . "\n",
 				( in_array( $interest_key, $input_interest_list ) ) ? 'selected="selected"' : '',
 				$interest_key,
 				$interest_value
@@ -59,75 +60,50 @@ class Template {
 		return $html;
 	}
 
-	public function save_academic_interests( $user_id ) {
-		$tax = get_taxonomy( 'mla_academic_interests' );
-
-		// If array add any new keywords.
-		if ( is_array( $_POST['academic-interests'] ) ) {
-			foreach ( $_POST['academic-interests'] as $term_id ) {
-				$term_key = term_exists( $term_id, 'mla_academic_interests' );
-				if ( empty( $term_key ) ) {
-					$term_key = wp_insert_term( sanitize_text_field( $term_id ), 'mla_academic_interests' );
-				}
-				if ( ! is_wp_error( $term_key ) ) {
-					$term_ids[] = intval( $term_key['term_id'] );
-				} else {
-					error_log( '*****CAC Academic Interests Error - bad tag*****' . var_export( $term_key, true ) );
-				}
-			}
-		}
-
-		// Set object terms for tags.
-		$term_taxonomy_ids = wp_set_object_terms( $user_id, $term_ids, 'mla_academic_interests' );
-		clean_object_term_cache( $user_id, 'mla_academic_interests' );
-
-		// Set user meta for theme query.
-		delete_user_meta( $user_id, 'academic_interests' );
-		foreach ( $term_taxonomy_ids as $term_taxonomy_id ) {
-			add_user_meta( $user_id, 'academic_interests', $term_taxonomy_id, $unique = false );
-		}
-	}
-
 	/**
 	 * for edit view. use like bp_the_profile_field().
 	 * works inside or outside the fields loop.
 	 * TODO optimize: find some way to look up fields directly rather than (re)winding the loop every time.
 	 */
 	public function get_edit_field( $field_name ) {
-		global $profile_template;
+		global $profile_template; // BP_XProfile_Data_Template
 
 		$profile_template->rewind_fields(); // reset the loop
 
-		while ( bp_profile_fields() ) {
-			bp_the_profile_field();
+		while ( \bp_profile_fields() ) {
+			\bp_the_profile_field();
 
-			if ( bp_get_the_profile_field_name() !== $field_name ) {
+			if ( \bp_get_the_profile_field_name() !== $field_name ) {
 				continue;
 			}
 
-			$field_type = bp_xprofile_create_field_type( bp_get_the_profile_field_type() );
+			$field_type = \bp_xprofile_create_field_type( \bp_get_the_profile_field_type() );
 			$field_type->edit_field_html();
 
-			do_action( 'bp_custom_profile_edit_fields_pre_visibility' );
-			bp_profile_visibility_radio_buttons();
+			\do_action( 'bp_custom_profile_edit_fields_pre_visibility' );
+			\bp_profile_visibility_radio_buttons();
 
-			do_action( 'bp_custom_profile_edit_fields' );
+			\do_action( 'bp_custom_profile_edit_fields' );
 			break; // once we output the field we want, no need to continue looping
 		}
 	}
 
+	/**
+	 * @uses DOMDocument
+	 */
 	public function get_activity( $max = 5 ) {
-		if ( bp_has_activities( bp_ajax_querystring( 'activity' ) . "&max=$max&scope=just-me" ) ) {
+		if ( \bp_has_activities( \bp_ajax_querystring( 'activity' ) . "&max=$max&scope=just-me" ) ) {
 
 			$actions_html = '';
 
-			while ( bp_activities() ) {
-				bp_the_activity();
+			while ( \bp_activities() ) {
+				\bp_the_activity();
 
-				$action = trim( strip_tags( bp_get_activity_action( [ 'no_timestamp' => true ] ), '<a>' ) );
-				$activity_type = bp_get_activity_type() ;
-				$displayed_user_fullname = bp_get_displayed_user_fullname();
+				$action = trim( strip_tags( \bp_get_activity_action( [ 'no_timestamp' => true ] ), '<a>' ) );
+				$activity_type = \bp_get_activity_type() ;
+				$displayed_user_fullname = \bp_get_displayed_user_fullname();
 				$link_text_char_limit = 30;
+
 				// shorten/change some action descriptions
 				switch ( $activity_type ) {
 				case 'updated_profile':
@@ -147,7 +123,11 @@ class Template {
 
 				// encoding prevents mangling of multibyte characters
 				// constants ensure no <body> or <doctype> tags are added
-				$action_doc->loadHTML( mb_convert_encoding( $action, 'HTML-ENTITIES', 'UTF-8' ), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+				$action_doc->loadHTML(
+					mb_convert_encoding( $action, 'HTML-ENTITIES', 'UTF-8' ),
+					LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD
+				);
+
 				// for reasons yet unknown, removeChild() causes the next anchor to be skipped entirely.
 				// using a second foreach is a workaround.
 				foreach ( $action_doc->getElementsByTagName( 'a' ) as $anchor ) {
@@ -161,38 +141,42 @@ class Template {
 						$anchor->nodeValue = substr( $anchor->nodeValue, 0, $link_text_char_limit - 1 ) . '…';
 					}
 				}
+
 				$action = $action_doc->saveHTML();
 
-				// only add unique actions
+				// only add actions which are unique in the feed
 				if ( empty( $actions_html ) || strpos( $action, $actions_html ) === false ) {
 					$actions_html .= $action;
 				}
 			}
 
-			echo "<ul>$actions_html</ul>";
+			return "<ul>$actions_html</ul>";
 		}
 	}
 
 	public function get_groups() {
-		if ( bp_has_groups( bp_ajax_querystring( 'groups' ) ) ) {
+		$html = '';
+
+		if ( \bp_has_groups( \bp_ajax_querystring( 'groups' ) ) ) {
 			$html = '<ul>';
-			while ( bp_groups() ) {
-				bp_the_group();
-				$html .= '<li><a href="' . bp_get_group_permalink() . '">' . bp_get_group_name() . '</a></li>';
+			while ( \bp_groups() ) {
+				\bp_the_group();
+				$html .= '<li><a href="' . \bp_get_group_permalink() . '">' . \bp_get_group_name() . '</a></li>';
 			}
 			$html .= '</ul>';
-			echo $html;
 		}
+
+		return $html;
 	}
 
 	public function get_sites() {
 		$html = '';
 
-		if ( bp_has_blogs( bp_ajax_querystring( 'blogs' ) ) ) {
+		if ( \bp_has_blogs( \bp_ajax_querystring( 'blogs' ) ) ) {
 			$html .= '<ul>';
-			while ( bp_blogs() ) {
-				bp_the_blog();
-				$html .= '<li><a href="' . bp_get_blog_permalink() . '">' . bp_get_blog_name() . '</a></li>';
+			while ( \bp_blogs() ) {
+				\bp_the_blog();
+				$html .= '<li><a href="' . \bp_get_blog_permalink() . '">' . \bp_get_blog_name() . '</a></li>';
 			}
 			$html .= '</ul>';
 		}
@@ -205,14 +189,16 @@ class Template {
 
 		$querystring = sprintf( 'facets[author_facet][]=%s', urlencode( bp_get_displayed_user_fullname() ) );
 
-		if ( humcore_has_deposits( $querystring ) ) {
+		if ( \humcore_has_deposits( $querystring ) ) {
 			$html = '<ul>';
-			while ( humcore_deposits() ) {
-				humcore_the_deposit();
-				$metadata = (array) humcore_get_current_deposit();
-				$item_url = sprintf( '%1$s/deposits/item/%2$s', bp_get_root_domain(), $metadata['pid'] );
-				$html .= '<li><a href="' . esc_url( $item_url ) . '/">' . $metadata['title_unchanged'] . '</a></li>';
+
+			while ( \humcore_deposits() ) {
+				\humcore_the_deposit();
+				$metadata = (array) \humcore_get_current_deposit();
+				$item_url = sprintf( '%1$s/deposits/item/%2$s', \bp_get_root_domain(), $metadata['pid'] );
+				$html .= '<li><a href="' . \esc_url( $item_url ) . '/">' . $metadata['title_unchanged'] . '</a></li>';
 			}
+
 			$html .= '</ul>';
 		}
 
@@ -227,8 +213,8 @@ class Template {
 
 		ob_start();
 
-		do_action( 'bp_member_header_actions' ); // buttons dependent on context
-		bp_get_options_nav(); // nav links, but we're grouping everything together
+		\do_action( 'bp_member_header_actions' ); // buttons dependent on context
+		\bp_get_options_nav(); // nav links, but we're grouping everything together
 
 		$html = ob_get_clean();
 
