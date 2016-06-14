930dbf8ecdb35244aed53555a08bdeed4916a2d5
Ryan Williams <ryrowi@gmail.com>
2016-06-14 17:05:23 -0400

    remove setup.bash

--- bash/setup.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-wp plugin deactivate --network cac-advanced-profiles
-wp plugin activate --network buddypress-followers
-wp plugin activate --network bp-block-member
-wp plugin activate --network profile
