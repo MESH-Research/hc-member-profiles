c76ec62a7ca26083623487562f48960f17968241
Ryan Williams <ryrowi@gmail.com>
2016-06-14 17:06:38 -0400

    update migrate.bash to accommodate new migrate script

--- bash/migrate.bash
+++ bash/migrate.bash
@@ -7,18 +7,9 @@ pre_php=/tmp/__pre.php
 wp="wp --path=/srv/www/commons/current/web/wp --require=$pre_php"
 
 $wp plugin deactivate --network cac-advanced-profiles
-$wp plugin activate --network mla-academic-interests
 $wp plugin activate --network buddypress-followers
 $wp plugin activate --network bp-block-member
+$wp plugin activate --network mla-academic-interests
 $wp plugin activate --network profile
 
-#if [[ "$PWD" != *plugins/profile ]]
-#then
-#  echo "you must run this from within the plugin root directory!"
-#  exit 1
-#fi
-
-$wp profile friends_to_followers
-$wp profile create_xprofile_fields
-$wp profile migrate_xprofile_field_data
-$wp profile migrate_academic_interests
+$wp profile migrate
