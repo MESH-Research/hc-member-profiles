b91dc33e38e0823835694c8b41e4e3d4f1745812
Ryan Williams <ryrowi@gmail.com>
2016-06-14 11:25:40 -0400

    add xprofile field & group csv export function

--- src/Profile/CLI.php
+++ src/Profile/CLI.php
@@ -207,6 +207,58 @@ class CLI {
 		}
 	}
 
+	/**
+	 * Export all xprofile field data + groups as CSV
+	 */
+	function export_xprofile_data() {
+		global $wpdb;
+
+		$groups = \BP_XProfile_Group::get( [ 'fetch_fields' => true ] );
+
+		$fields = $groups[0]->fields;
+
+		$user_id = 1;
+		$max_user_id = $wpdb->get_var( 'SELECT MAX(ID) FROM wp_users' );
+
+		$fp = fopen( 'xprofile_export.csv', 'w' );
+
+		// column headers
+		$row = [];
+		foreach ( $fields as $field ) {
+			$row[] = $field->name;
+		}
+		$row[] = 'Group Slugs';
+		fputcsv( $fp, $row );
+
+		// data rows
+		while ( $user_id <= $max_user_id ) {
+			$userdata = \get_userdata( $user_id );
+
+			if ( $userdata ) {
+				$row = [];
+
+				foreach ( $fields as $field ) {
+					$row[] = \xprofile_get_field_data( $field->id, $user_id );
+				}
+
+				$group_ids = \groups_get_user_groups( $user_id );
+				$group_slugs = [];
+
+				foreach ( $group_ids as $group_id ) {
+					$group_slugs[] = \groups_get_slug( $group_id );
+				}
+
+				$row[] = implode( ';', $group_slugs );
+
+				fputcsv( $fp, $row );
+			}
+
+			$user_id++;
+		}
+
+		fclose( $fp );
+	}
+
 }
 
 if( defined( 'WP_CLI' ) && WP_CLI ) {
