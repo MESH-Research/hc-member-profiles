9492ad6ea4ebfcd62c98e5cc005f46716898a7e2
Ryan Williams <ryrowi@gmail.com>
2016-06-24 16:43:48 -0400

    improve scope & modularity of main js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 /**
  * MLA Commons Profile
  */
-( function( $ ) {
-  $( document ).ready( function() {
 
-    var init_visibility_controls = ( function() {
-      $( '#profile-edit-form .editable' ).each( function() {
+( function( $ ) {
+
+  window.mla_commons_profile = {
+
+    init: function() {
+      // visibility controls
+       $( '#profile-edit-form .editable' ).each( function() {
         var div = $( this );
 
         // add visibility controls
@@ -34,15 +37,22 @@
           div.find( '.visibility' ).triggerHandler( 'click' );
         }
       } );
-    } )();
 
-    // cancel button to send user back to view mode
-    var init_cancel_button = ( function() {
+      // cancel button to send user back to view mode
       $( '#profile-edit-form #cancel' ).click( function( e ) {
         e.preventDefault();
         window.location = $( '#public' ).attr( 'href' );
       } );
-    } )();
 
-  } );
+      $( '#profile-edit-form input' ).on( 'change', mla_commons_profile.editor_change_handler );
+    },
+
+    editor_change_handler: function() {
+      $( '.bp-template-notice.updated' ).slideUp();
+    }
+
+  }
+
+  $( document ).ready( mla_commons_profile.init );
+
 } )( jQuery );
