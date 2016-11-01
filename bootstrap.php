b251d0a2e951fa527058cbe23eb9073ab38dc1d4
Ryan Williams <ryrowi@gmail.com>
2016-11-01 15:16:16 -0400

    format profile update activities to indicate which field(s) changed

--- bootstrap.php
+++ bootstrap.php
@@ -9,3 +9,6 @@ require_once 'autoload.php';
 
 // initialize actions & filters etc. by instantiating
 Profile::get_instance();
+
+// since Profile hooks into bp_init, add this separately since this action runs before that
+add_action( 'xprofile_register_activity_actions', [ '\MLA\Commons\Profile\Activity', 'register_activity_actions' ] );
