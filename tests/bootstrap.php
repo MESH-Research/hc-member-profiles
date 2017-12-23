34a74c12068cbd3ebb2793dc8492078b2c8eb93e
Ryan Williams <ryrowi@gmail.com>
2017-12-23 17:03:13 -0500

    add wp plugin scaffold files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,44 +1,30 @@
 <?php
 /**
- * Bootstraps WordPress, BuddyPress, and your code so that you can run tests on
- * it. The function `_manually_load_our_code` is probably the only section you
- * need to edit when adapting this for new projects.
+ * PHPUnit bootstrap file
+ *
+ * @package Hc_Member_Profiles
  */
 
-// Get codebase versions.
-$wp_version = ( getenv( 'WP_VERSION' ) ) ? getenv( 'WP_VERSION' ) : 'latest';
-$bp_version = ( getenv( 'BP_VERSION' ) ) ? getenv( 'BP_VERSION' ) : 'latest';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
-// Get paths to codebase installed by install script.
-$wp_root_dir = "/tmp/wordpress/$wp_version/src/";
-$wp_tests_dir = "/tmp/wordpress/$wp_version/tests/phpunit";
-$bp_tests_dir = "/tmp/buddypress/$bp_version/tests/phpunit";
-
-// Set required environment variables.
-putenv( 'WP_ABSPATH=' . $wp_root_dir );
-putenv( 'WP_TESTS_DIR=' . $wp_tests_dir );
-putenv( 'BP_TESTS_DIR=' . $bp_tests_dir );
-
-// Let code know we are running tests.
-define( 'RUNNING_TESTS', true );
-
-// Load WordPress.
-require_once $wp_tests_dir . '/includes/functions.php';
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
 
 /**
- * Load WordPress, BuddyPress, testable plugin code, and mocks.
+ * Manually load the plugin being tested.
  */
-function _manually_load_our_code() {
+function _manually_load_plugin() {
 
 	// Load BuddyPress.
 	require_once getenv( 'BP_TESTS_DIR' ) . '/includes/loader.php';
 
 	// Load plugin classes.
 	require_once dirname( __FILE__ ) . '/../bootstrap.php';
-
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_our_code' );
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Bootstrap tests.
-require_once $wp_tests_dir . '/includes/bootstrap.php';
-require_once $bp_tests_dir . '/includes/testcase.php';
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
