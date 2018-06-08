6a3d6c117b176cd484d068b97a29c46d0dfbcbb7
Ryan Williams <rwilliams@mla.org>
2018-06-08 10:31:39 -0400

    clean up migrate

--- bin/migrate.php
+++ bin/migrate.php
@@ -8,17 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 // For some reason (BP bug?) this is not correctly populated with the filtered value during eval-file - force it here.
 buddypress()->profile->field_types = array_keys( hcmp_register_xprofile_field_types( array_keys( buddypress()->profile->field_types ) ) );
 
-//$delete_all_fields = function() {
-//	for ( $i = 1; $i < 30; $i++ ) {
-//		xprofile_delete_field( $i );
-//	}
-//	for ( $i = 1000000; $i < 1000030; $i++ ) {
-//		xprofile_delete_field( $i );
-//	}
-//};
-//$delete_all_fields();
-//die;
-
 // Update real "Name" field to use correct name.
 $name_field = xprofile_get_field( 1 );
 $name_field->name = 'Name';
@@ -60,6 +49,7 @@ $result = _hcmp_create_xprofile_fields();
 
 /*
 
+Expected before:
 
 +---------+---------------------------------------+----------------------------------------+---------------+----------+-------------+
 | id      | name                                  | description                            | type          | group_id | is_required |
@@ -87,5 +77,34 @@ $result = _hcmp_create_xprofile_fields();
 | 22      | Projects                              |                                        | textarea      | 2        | 0           |
 | 23      | Upcoming Talks and Conferences        |                                        | textarea      | 2        | 0           |
 ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+
+Expected after:
+
++---------+------------------------------------+-------------+--------------------+----------+-------------+
+| id      | name                               | description | type               | group_id | is_required |
++---------+------------------------------------+-------------+--------------------+----------+-------------+
+| 1       | Name                               |             | textbox            | 1        | 1           |
+| 14      | Institutional or Other Affiliation |             | textbox            | 1        | 0           |
+| 15      | Title                              |             | textbox            | 1        | 0           |
+| 16      | Site                               |             | textbox            | 1        | 0           |
+| 17      | <em>Twitter</em> handle            |             | textbox            | 1        | 0           |
+| 18      | <em>ORCID</em> iD                  |             | textbox            | 1        | 0           |
+| 19      | About                              |             | textarea           | 1        | 0           |
+| 20      | Education                          |             | textarea           | 1        | 0           |
+| 21      | Other Publications                 |             | textarea           | 1        | 0           |
+| 22      | Projects                           |             | textarea           | 1        | 0           |
+| 23      | Upcoming Talks and Conferences     |             | textarea           | 1        | 0           |
+| 24      | Memberships                        |             | textarea           | 1        | 0           |
+| 1000025 | Facebook URL                       |             | url                | 1        | 0           |
+| 1000026 | LinkedIn URL                       |             | url                | 1        | 0           |
+| 1000027 | Website URL                        |             | url                | 1        | 0           |
+| 1000028 | CORE Deposits                      |             | core_deposits      | 1        | 0           |
+| 1000029 | CV                                 |             | bp_attachment      | 1        | 0           |
+| 1000030 | Academic Interests                 |             | academic_interests | 1        | 0           |
+| 1000031 | Commons Groups                     |             | bp_groups          | 1        | 0           |
+| 1000032 | Recent Commons Activity            |             | bp_activity        | 1        | 0           |
+| 1000033 | Commons Sites                      |             | bp_blogs           | 1        | 0           |
++---------+------------------------------------+-------------+--------------------+----------+-------------+
 
 */
